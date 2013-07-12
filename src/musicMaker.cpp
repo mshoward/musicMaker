0b41a4b93960b35f8384f9967803c1801963aa4e
mshoward <mshoward42@gmail.com>
2013-07-12 13:48:00 -0500

    Update musicMaker.cpp

--- src/musicMaker.cpp
+++ src/musicMaker.cpp
@@ -1,1 +1,23 @@
-//Mason Howard
+#include <iostream>
+#include <chrono>
+#include <random>
+
+Song::Song()
+{
+  themes = 0;
+	length = 0;
+	positions = 0;
+	seed = std::chrono::system_clock::now().time_since_epoch().count()
+}
+
+Song::~Song()
+{
+}
+
+Song::genSong()
+{
+}
+
+Song::writeSong()
+{
+}
