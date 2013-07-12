38e4b9ba5ad1f3b654d167250b33d91bfb3af23c
mshoward <mshoward42@gmail.com>
2013-07-12 13:34:17 -0500

    Update musicMaker.hpp

--- src/musicMaker.hpp
+++ src/musicMaker.hpp
@@ -1,1 +1,31 @@
-//Mason Howard
+#include <iostream>
+#include <chrono>
+#include <random>
+
+struct Note
+{
+	char note;
+	char length;
+};
+
+struct Theme
+{
+	Note * notes;
+	int length;
+	int position;
+};
+
+class Song
+{
+	public:
+		Theme * themes;
+		int length;
+		int * positions;
+		unsigned seed;
+		
+		Song();
+		~Song();
+		
+		void genSong();
+		void writeSong();
+}
